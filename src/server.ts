25773bfa1c6f05b47e149506abb619484e832c35
Jorne Saenen <59235736+JorneSaenen@users.noreply.github.com>
2025-03-26 11:24:42 +0100

    added multer

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import chalk from "chalk";
 import { PORT } from "./utils/envs";
+import multer from "multer";
+import path from "path";
 
 // Variables
 const app = express();
@@ -11,15 +13,40 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("src/public"));
+app.use("/uploads", express.static("uploads"));
+
+const storage = multer.diskStorage({
+  destination: "./uploads",
+  filename: function (req, file, cb) {
+    const newName =
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname);
+    cb(null, newName);
+  },
+});
+const upload = multer({ storage: storage, limits: { fileSize: 3000000 } });
 
 // Routes
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    res.status(400).send("No file uploaded");
+    return;
+  }
+
+  const imageUrl = `/uploads/${req.file.filename}`;
+  res.status(200).send(`
+    <h1>Image uploaded successfully</h1>
+    <img src="http://localhost:3000${imageUrl}" width="500"/>
+    `);
+});
+
 app.get("/", (req, res) => {
   res.sendFile("/index.html");
 });
 
 // Foutafhandeling middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).send(err.message);
+  res.status(500).json({ message: err.message });
 });
 
 // Server Listening
